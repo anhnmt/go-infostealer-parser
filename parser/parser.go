054cfe073b7c2856cf5ba3bee14432176331ce64
Tuan Anh <anhng.dev@gmail.com>
2024-08-30 23:39:23 +0700

    Add ParseUserInfo

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"github.com/anhnmt/go-infostealer-parser/parser/credential"
 	"github.com/anhnmt/go-infostealer-parser/parser/extract"
 	"github.com/anhnmt/go-infostealer-parser/parser/model"
+	"github.com/anhnmt/go-infostealer-parser/parser/userinfo"
 )
 
 func ParseCredentials(filePath, outputDir string, passwords ...string) ([]*model.Credential, error) {
@@ -13,4 +14,13 @@ func ParseCredentials(filePath, outputDir string, passwords ...string) ([]*model
 	}
 
 	return credential.DetectStealer(files), nil
+}
+
+func ParseUserInfo(filePath, outputDir string, passwords ...string) ([]*model.UserInformation, error) {
+	files, err := extract.ExtractFile(filePath, outputDir, passwords...)
+	if err != nil {
+		return nil, err
+	}
+
+	return userinfo.DetectStealer(files), nil
 }
